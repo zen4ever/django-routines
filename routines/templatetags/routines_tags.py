0d043cc844940c3c7927456a4f9e7380232c6c7e
Andrii Kurinnyi <prigun@gmail.com>
2009-02-21 02:49:23 -0800

    Added tag for displaing integer number in form of minutes and seconds

--- routines/templatetags/routines_tags.py
+++ routines/templatetags/routines_tags.py
@@ -7,3 +7,15 @@ def startswith(value, arg):
     """ Usage, {% if value|starts_with:"arg" %}"""
     return value.startswith(arg)
 
+@register.filter
+def minutes_from_seconds(value, arg=False):
+    value = int(value)
+    mins = value / 60
+    secs = value % 60
+    result = ""
+    from django.template.defaultfilters import pluralize
+    if mins:
+        result+="%d minute" % mins + pluralize(mins)
+    if not arg or secs:
+        result+=" %d second" % secs + pluralize(secs)
+    return result
