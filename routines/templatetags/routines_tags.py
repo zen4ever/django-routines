cc9cb4fcc3a0063e786eb40b965ddf96530ce8a5
Andrii Kurinnyi <prigun@gmail.com>
2009-04-05 08:06:52 +0000

    Removed minutes_from_seconds tags, because it is too specific to needs of a project

--- routines/templatetags/routines_tags.py
+++ routines/templatetags/routines_tags.py
@@ -12,22 +12,6 @@ def startswith(value, arg):
     return value.startswith(arg)
 
 @register.filter
-def minutes_from_seconds(value, arg=False):
-    if not value:
-        value=0
-    value = int(value)
-    mins = value / 60
-    secs = value % 60
-    result = ""
-    from django.template.defaultfilters import pluralize
-    if mins:
-        result+='<span class="minutes">%d</span> minute' % mins + pluralize(mins)
-    if not arg or secs:
-        result+=' <span class="seconds">%d</span> second' % secs + pluralize(secs)
-    from django.utils.safestring import mark_safe 
-    return mark_safe(result)
-
-@register.filter
 def truncatechars(s, num=15):
     length = int(num)
     string = []
