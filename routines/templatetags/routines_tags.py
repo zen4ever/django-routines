96659e236b0dba74b7ac3413dbccdb21affcd0e4
Andrii Kurinnyi <prigun@gmail.com>
2009-04-05 08:01:09 +0000

    Added template tag for handling static files

--- routines/templatetags/routines_tags.py
+++ routines/templatetags/routines_tags.py
@@ -1,4 +1,8 @@
 from django import template
+import urlparse
+import os.path
+from django.conf import settings
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -33,4 +37,27 @@ def truncatechars(s, num=15):
         else:
             string.append(word)
     return u' '.join(string)
+
+def _absolute_url(url):
+    if url.startswith('http://') or url.startswith('https://'):
+        return url
+    domain = Site.objects.get_current().domain
+    return 'http://%s%s' % (domain, url)
+
+@register.simple_tag
+def static(filename, flags=''):
+    """
+    Handles urls to static files, adds timestamp to js and css files.
+    Taken from Ivan Salagaev http://softwaremaniacs.org/ 
+    """
+    flags = set(f.strip() for f in flags.split(','))
+    url = urlparse.urljoin(settings.STATIC_URL, filename)
+    if 'absolute' in flags:
+        url = _absolute_url(url)
+    if (filename.endswith('.css') or filename.endswith('.js')) and 'no-timestamp' not in flags or \
+       'timestamp' in flags:
+        fullname = os.path.join(settings.STATIC_ROOT, filename)
+        if os.path.exists(fullname):
+            url += '?%d' % os.path.getmtime(fullname)
+    return url
 
